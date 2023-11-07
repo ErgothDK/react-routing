98c129f76ab6c1dd64f66e1359e1d842c4eeec71
ErgothDK <ergothdk@gmail.com>
2023-11-07 12:41:27 -0600

    Added second page routing

---
+++ src/pages/Products.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <h1>Products List</h1>
+      <ul>
+        <li>First product</li>
+        <li>Second product</li>
+        <li>Third product</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Products;
